8c11133a39f132dd60c9158089b2d4c312182217
Fakyouka <maksimstar0808@gmail.com>
2024-05-20 20:34:37 +0400

    ya sozdal kolonu

--- main.cpp
+++ main.cpp
@@ -1,13 +1,27 @@
 #include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
 
-using namespase std;
-
-const int N = 10
+const int N = 3;
+const int C = 1;
 
 int main() {
 
-	cout << N;
-	cout << "ÁÅÁÁÅÁÅÁÅÁÅÁÅÁÁÅÁÅÁÅÁÅ ÁÀÁÀÁÀÁÀÁÀÁÀÁÀÁÁÀ";
+    string weightFile = "10";
+    string nameFile = "main";
+    string typeFile = ".cpp";
 
-	return 0;
+    cout << "FileName" << " " << "Type" << " " << "Weight" << endl;
+    cout << endl;
+    string mass[C][N] = { {nameFile, typeFile, weightFile} };
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < C; j++) {
+            cout << mass[j][i];
+            cout << "    ";
+        }
+    }
+
+    return 0;
 }
