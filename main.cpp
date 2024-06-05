23a150d7c56e93c50e467735cb3c0e6b5611dc41
Fakyouka <maksimstar0808@gmail.com>
2024-06-05 20:12:25 +0400

    v1.2

--- main.cpp
+++ main.cpp
@@ -1,38 +1,53 @@
 #include <iostream>
-#include <filesystem>
-#include <cstdlib>
 #include <fstream>
+#include <filesystem>
+#include <cstring>
 
 using namespace std;
 namespace fs = filesystem;
 using namespace fs;
 
-path folderPath;
-string folderName;
-
 ofstream fout;
 ifstream fin;
 
-void userChoiceSpace();
+path folderPath;
+string folderName;
+string userPath;
+bool userOn = true;
+
+//         Функционал
+// ---------------------------- //
+
+void menu();
+int main();
 
 void userChoiceFolder() {
     int choice;
+
+    system("cls");
     cout << " " << endl;
     cout << "Вы работаете в папке: " + folderName << endl;
     cout << " " << endl;
 
-    cout << "Выберите действие: \n 1. Создать файл в папке \n 2. Удалить файл в папке \n 3. Создание или выбор другой папки \n 4. Ввод данных в файл \n 5. Переход в пространство \n 6. Список всех файлов в папке \n 0. Выход" << endl;
+    cout << "Выберите действие: \n 1. Создать файл в папке \n 2. Удалить файл в папке \n 3. Создание или выбор другой папки \n 4. Ввод данных в файл \n 5. Список всех файлов в папке \n 6. Меню \n 0. Выход" << endl;
+    cout << " " << endl;
+    cout << "> ";
     cin >> choice;
 
     if (choice == 1) {
+        system("cls");
         string fileName;
         string Expansion;
 
         cout << " " << endl;
         cout << "Введите название файла: ";
+        cout << " " << endl;
+        cout << "> ";
         cin >> fileName;
 
         cout << "Введите расширение файла: ";
+        cout << " " << endl;
+        cout << "> ";
         cin >> Expansion;
 
         fout.open(folderName + "/" + fileName + "." + Expansion);
@@ -48,25 +63,33 @@ void userChoiceFolder() {
         }
     }
     else if (choice == 2) {
+        system("cls");
         string fileName;
         string Expansion;
 
         cout << " " << endl;
         cout << "Введите название файла, который хотите удалить: ";
+        cout << " " << endl;
+        cout << "> ";
         cin >> fileName;
 
         cout << "Введите его разрешение: ";
+        cout << " " << endl;
+        cout << "> ";
         cin >> Expansion;
 
         remove(folderName + "/" + fileName + "." + Expansion);
         cout << " " << endl;
         cout << "Файл успешно был удалён!" << endl;
-        
+
         userChoiceFolder();
     }
     else if (choice == 3) {
+        system("cls");
         cout << "Напишите название папки: " << endl;
         cout << "\nЕсли папка уже существует, то вы просто в неё войдёте!" << endl;
+        cout << " " << endl;
+        cout << "> ";
         cin >> folderName;
 
         path folderPath = current_path() / folderName;
@@ -84,14 +107,19 @@ void userChoiceFolder() {
         }
     }
     else if (choice == 4) {
+        system("cls");
         string fileName;
         string Expansion;
 
         string enter;
 
         cout << "Введите название файла: ";
+        cout << " " << endl;
+        cout << "> ";
         cin >> fileName;
         cout << "Введите расширение файла: ";
+        cout << " " << endl;
+        cout << "> ";
         cin >> Expansion;
 
         fout.open(folderName + "/" + fileName + "." + Expansion);
@@ -111,17 +139,14 @@ void userChoiceFolder() {
         }
         else {
             cout << "Err: Файл не был открыт или создан!" << endl;
-            
+
             userChoiceFolder();
 
             fout.close();
         }
     }
     else if (choice == 5) {
-        cout << "\n Вы успешно перешли в пространство!" << endl;
-        userChoiceSpace();
-    }
-    else if (choice == 6) {
+        system("cls");
         string path = folderName + "/";
 
         cout << "Список файлов в папке (Кроме папок): " << endl;
@@ -133,35 +158,66 @@ void userChoiceFolder() {
             }
         }
 
-        userChoiceFolder();
+        int number;
+        bool whileOn = true;
+        while (whileOn == true) {
+            cout << "\nВведите любую цифру, чтобы закрыть: " << endl;
+            cout << "> ";
+
+            cin >> number;
+            if (number >= 0) {
+                whileOn = false;
+                userChoiceFolder();
+            }
+            else if (number < 0) {
+                whileOn = false;
+                userChoiceFolder();
+            }
+            else {
+                cout << "Err: Это не цифра!" << endl;
+            }
+        }
+    }
+    else if (choice == 6) {
+        system("cls");
+        menu();
     }
     else if (choice == 0) {
         cout << "Exit";
     }
     else {
+        system("cls");
         cout << "Err: Неизвестное действие!";
         userChoiceFolder();
     }
 }
-
 void userChoiceSpace() {
     int choice;
+    system("cls");
+
     cout << " " << endl;
     cout << "Вы работаете в пространстве" << endl;
     cout << " " << endl;
 
-    cout << "Выберите действие: \n 1. Создать файл \n 2. Удалить файл \n 3. Переход в папки \n 4. Ввод данных в файл \n 5. Список всех файлов в пространстве\n 0. Выход" << endl;
+    cout << "Выберите действие: \n 1. Создать файл \n 2. Удалить файл \n 3.Ввод данных \n 4. Список всех файлов в пространстве \n 5. Меню \n 0. Выход" << endl;
+    cout << " " << endl;
+    cout << "> ";
     cin >> choice;
 
     if (choice == 1) {
+        system("cls");
         string fileName;
         string Expansion;
 
         cout << " " << endl;
         cout << "Введите название файла: ";
+        cout << " " << endl;
+        cout << "> ";
         cin >> fileName;
 
         cout << "Введите расширение файла: ";
+        cout << " " << endl;
+        cout << "> ";
         cin >> Expansion;
 
         fout.open(fileName + "." + Expansion);
@@ -177,14 +233,19 @@ void userChoiceSpace() {
         }
     }
     else if (choice == 2) {
+        system("cls");
         string fileName;
         string Expansion;
 
         cout << " " << endl;
         cout << "Введите название файла, который хотите удалить: ";
+        cout << " " << endl;
+        cout << "> ";
         cin >> fileName;
 
         cout << "Введите его разрешение: ";
+        cout << " " << endl;
+        cout << "> ";
         cin >> Expansion;
 
         remove(fileName + "." + Expansion);
@@ -194,33 +255,19 @@ void userChoiceSpace() {
         userChoiceSpace();
     }
     else if (choice == 3) {
-        cout << "Напишите название папки: " << endl;
-        cout << "\nЕсли папка уже существует, то вы просто в неё войдёте!" << endl;
-        cin >> folderName;
-
-        path folderPath = current_path() / folderName;
-
-        if (!exists(folderPath)) {
-            create_directory(folderPath);
-            cout << "Успешно!\n";
-
-            userChoiceFolder();
-        }
-        else {
-            cout << "Вы успешно вошли в папку " + folderName;
-
-            userChoiceFolder();
-        }
-    }
-    else if (choice == 4) {
+        system("cls");
         string fileName;
         string Expansion;
 
         string enter;
 
         cout << "Введите название файла: ";
+        cout << " " << endl;
+        cout << "> ";
         cin >> fileName;
         cout << "Введите расширение файла: ";
+        cout << " " << endl;
+        cout << "> ";
         cin >> Expansion;
 
         fout.open(fileName + "." + Expansion);
@@ -246,7 +293,8 @@ void userChoiceSpace() {
             fout.close();
         }
     }
-    else if (choice == 5) {
+    else if (choice == 4) {
+        system("cls");
         string path = ".";
 
         cout << "Список файлов (Кроме папок): " << endl;
@@ -258,7 +306,30 @@ void userChoiceSpace() {
             }
         }
 
-        userChoiceSpace();
+        int number;
+        bool whileOn = true;
+        while (whileOn == true) {
+            cout << "\nВведите любую цифру, чтобы закрыть: " << endl;
+            cout << "> ";
+
+            cin >> number;
+            if (number >= 0) {
+                whileOn = false;
+                userChoiceSpace();
+            }
+            else if (number < 0) {
+                whileOn = false;
+                userChoiceSpace();
+            }
+            else {
+                cout << "Err: Это не цифра!" << endl;
+            }
+        }
+
+    }
+    else if (choice == 5) {
+        system("cls");
+        menu();
     }
     else if (choice == 0) {
         cout << "Exit";
@@ -268,64 +339,232 @@ void userChoiceSpace() {
         userChoiceSpace();
     }
 }
-
-int main() {
+void userChoicePath() {
     int choice;
-    setlocale(LC_ALL, "Russian");
-
-    cout << "==============================" << endl;
-    cout << "|                            |" << endl;
-    cout << "|       FILEDATABASE         |" << endl;
-    cout << "|   by Fakyouka & nikon_05   |" << endl;
-    cout << "|                            |" << endl;
-    cout << "==============================" << endl;
+    system("cls");
 
     cout << " " << endl;
+    cout << "Вы работаете по пути: " + userPath << endl;
+    cout << " " << endl;
 
-    cout << "Выберете рабочее место: \n 1. В папке \n 2. В пространстве \n 3. Авторы" << endl;
+    cout << "Выберите действие: \n 1. Создать файл в папке \n 2. Удалить файл в папке \n 3. Выбор другого пути \n 4. Ввод данных в файл \n 5. Список всех файлов в папке \n 6. Меню \n 0. Выход" << endl;
+    cout << " " << endl;
+    cout << "> ";
+    cin >> choice;
+
+    if (choice == 1) {
+        system("cls");
+        string fileName;
+        string Expansion;
+
+        cout << " " << endl;
+        cout << "Введите название файла: ";
+        cout << " " << endl;
+        cout << "> ";
+        cin >> fileName;
+
+        cout << "Введите расширение файла: ";
+        cout << " " << endl;
+        cout << "> ";
+        cin >> Expansion;
+
+        fout.open(userPath + "/" + fileName + "." + Expansion);
+        if (!fout.is_open()) {
+            cout << "Err: Файл не был создан!" << endl;
+            userChoicePath();
+        }
+        else {
+            cout << "Файл с названием: " + fileName + "." + Expansion + " был успешно создан по пути " + userPath << endl;
+
+            fout.close();
+            userChoicePath();
+        }
+    }
+    else if (choice == 2) {
+        system("cls");
+        string fileName;
+        string Expansion;
+
+        cout << " " << endl;
+        cout << "Введите название файла, который хотите удалить: ";
+        cout << " " << endl;
+        cout << "> ";
+        cin >> fileName;
+
+        cout << "Введите его разрешение: ";
+        cout << " " << endl;
+        cout << "> ";
+        cin >> Expansion;
+
+        remove(userPath + "/" + fileName + "." + Expansion);
+        cout << " " << endl;
+        cout << "Файл успешно был удалён!" << endl;
+
+        userChoicePath();
+    }
+    else if (choice == 3) {
+        system("cls");
+        cout << "Впишите путь в котором будете работать: " << endl;
+        cout << "Пример: C:/Папка/Папка" << endl;
+
+        cout << " " << endl;
+        cout << "> ";
+        cin >> userPath;
+
+        userChoicePath();
+    }
+    else if (choice == 4) {
+        system("cls");
+        string fileName;
+        string Expansion;
+
+        string enter;
+
+        cout << "Введите название файла: ";
+        cout << " " << endl;
+        cout << "> ";
+        cin >> fileName;
+        cout << "Введите расширение файла: ";
+        cout << " " << endl;
+        cout << "> ";
+        cin >> Expansion;
+
+        fout.open(userPath + "/" + fileName + "." + Expansion);
+        if (fout.is_open()) {
+            cout << "Файл успешно открыт! (Если имя указано не верное, то файл был создан)" << endl;
+            cout << "Внимание! Ввод данных не работает с пробелами!" << endl;
+            cout << "\n> ";
+
+            cin >> enter;
+            fout << enter;
+
+            cout << "Данные были введены!" << endl;
+            userChoicePath();
+
+            fout.close();
+
+        }
+        else {
+            cout << "Err: Файл не был открыт или создан!" << endl;
+
+            userChoicePath();
+
+            fout.close();
+        }
+    }
+    else if (choice == 5) {
+        system("cls");
+        string path = userPath + "/";
+
+        cout << "Список файлов в папке (Кроме папок): " << endl;
+        cout << " " << endl;
+
+        for (const auto& entry : directory_iterator(path)) {
+            if (entry.is_regular_file()) {
+                cout << entry.path().filename() << endl;
+            }
+        }
+        int number;
+        bool whileOn = true;
+        while (whileOn == true) {
+            cout << "\nВведите любую цифру, чтобы закрыть: " << endl;
+            cout << "> ";
+
+            cin >> number;
+            if (number >= 0) {
+                whileOn = false;
+                userChoicePath();
+            }
+            else if (number < 0) {
+                whileOn = false;
+                userChoicePath();
+            }
+            else {
+                cout << "Err: Это не цифра!" << endl;
+            }
+        }
+        
+    }
+    else if (choice == 6) {
+        system("cls");
+        menu();
+    }
+    else if (choice == 0) {
+        cout << "Exit";
+    }
+    else {
+        cout << "Err: Неизвестное действие!";
+        userChoicePath();
+    }
+}
+
+//            Меню
+// ---------------------------- // 
+
+void menu() {
+    int choice;
+
+    cout << " " << endl;
+    cout << "Выберете рабочее место: \n 1. Работа в Папке \n 2. Работа в Пространстве \n 3. Работа по Пути \n 4. Выход" << endl;
+    cout << "> ";
     cin >> choice;
 
     if (choice == 1) {
         cout << "Напишите название папки: " << endl;
         cout << "\nЕсли папка уже существует, то вы просто в неё войдёте!" << endl;
+
+        cout << " " << endl;
+        cout << "> ";
         cin >> folderName;
 
         path folderPath = current_path() / folderName;
 
         if (!exists(folderPath)) {
             create_directory(folderPath);
-            cout << "Успешно!\n";
+            cout << "Папка была успешно создана!\n";
 
             userChoiceFolder();
         }
         else {
-            cout << "Вы успешно вошли в папку" << endl;
+            cout << "Вы успешно вошли в папку!" << endl;
 
             userChoiceFolder();
         }
     }
     else if (choice == 2) {
+        cout << "Вы перешли в пространство!" << endl;
+
         userChoiceSpace();
     }
     else if (choice == 3) {
-        int back;
-        cout << "Авторы приложения: Fakyouka(Максим) & nikon_05(Никита)" << endl;
+        cout << "Впишите путь в котором будете работать: " << endl;
+        cout << "Пример: C:/Папка/Папка" << endl;
+
         cout << " " << endl;
-        cout << "Введите любое число, чтобы выйти: ";
+        cout << "> ";
+        cin >> userPath;
 
-        cin >> back;
-
-        if (back >= 0) {
-            main();
-        }
-        else {
-            main();
-        }
+        userChoicePath();
+    }
+    else if (choice == 4) {
+        cout << "Exit";
     }
     else {
-        cout << "Err: Неизвестное пространство!" << endl;
-        main();
+        cout << "Err: Неккоректное значение!" << endl;
+        menu();
     }
+}
 
-    return 0;
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+    cout << "|=======================================================|" << endl;
+    cout << "|                                                       |" << endl;
+    cout << "|                  FILEDATABASE v1.2                    |" << endl;
+    cout << "|      by Fakyouka(Максим) & nikon_05(Никита)           |" << endl;
+    cout << "|                                                       |" << endl;
+    cout << "|=======================================================|" << endl;
+    menu();
+
+	return 0;
 }
